7506e5c66fc02d146bf252ce3aee6adf5defa7a8
ADeliriousKoala <cwiley39240@jcss.us>
2016-01-20 13:15:48 -0500

    Created by Coding Ground Client

---
+++ New Project/Animal.java
@@ -0,0 +1,12 @@
+public class Animal 
+{ 
+   protected int age
+   
+   private Animal  (int age1)
+   {age=age1;}
+   
+   public int getAge()
+   {return age;}
+   
+   public void setAge (int age1)
+   {age = age1;}
