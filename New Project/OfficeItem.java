2717f5433adcda3d649f5bfc0aaec8157aa40f42
ADeliriousKoala <cwiley39240@jcss.us>
2016-01-26 13:13:41 -0500

    Created by Coding Ground Client

---
+++ New Project/OfficeItem.java
@@ -0,0 +1,11 @@
+public class officeItem
+{
+    protected double cost;
+    
+    public officeItem(double cost1){
+    cost = cost1;
+        }
+    public void setCost(double new Cost){
+        cost = newCost;
+    }
+}
